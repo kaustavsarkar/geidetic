17bc13c1463df343c6530d943ee34f6bba247758
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2023-11-26 22:11:59 +0530

    Updating name of the project to ain

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 """Setup folder."""
 from setuptools import setup, find_packages
 
-setup(name='myproject', version='1.0', packages=find_packages())
+setup(name='ain', version='1.0', packages=find_packages())
